fc5bcfd2a18d4bc72c70d6f4cc369db288452fc5
mehak verma <mehakverma1610@gmail.com>
2026-04-22 12:36:15 +0530

    fixed vercel

--- apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GolfPlatformConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'golf_platform'
--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-*=%)ez%zyxk^jx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.vercel.app').split(',')
 
 
 # Application definition
